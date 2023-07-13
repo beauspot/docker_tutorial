93a5efcf3670b2009e4644af4bc5f7455aa14a84
beauspot <beauspot@outlook.com>
2023-07-13 22:36:42 +0100

    brought in some dependencies

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+
 const app = express();
+app.use(logger("dev"));
+app.use(cors());
+app.use(helmet());
+
 const port = 3000;
 
 app.get("/", (req, res) =>
@@ -11,6 +19,36 @@ app.get("/", (req, res) =>
         age: 26,
         gender: "male",
         monthlyIncome: 500000,
+        isDeveloper: true,
+      },
+    },
+    {
+      userData: {
+        name: "Iyere Paul Akhuomo",
+        email: "akhuomo@gmail.com",
+        age: 22,
+        gender: "male",
+        monthlyIncome: 50000,
+        isDeveloper: true,
+      },
+    },
+    {
+      userData: {
+        name: "Omoyemi Empress",
+        email: "empress@yahoo.com.com",
+        age: 29,
+        gender: "female",
+        monthlyIncome: 5000000,
+        isDeveloper: true,
+      },
+    },
+    {
+      userData: {
+        name: "testing user",
+        email: "testinguser@yahoo.com.com",
+        age: 20,
+        gender: "female",
+        monthlyIncome: 5000,
         isDeveloper: true,
       },
     },
